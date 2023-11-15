c81131b4978fb64eaaf861dca43fe1015d37603f
조윤환 <jyh6314@naver.com>
2023-11-15 18:21:00 +0900

    todo 디자인 완료

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import { StatusBar } from "expo-status-bar";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import {
   StyleSheet,
   Text,
@@ -9,9 +10,12 @@ import {
   Pressable,
   TextInput,
   ScrollView,
+  Alert,
 } from "react-native";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { theme } from "./color";
+
+const STOAGE_KEY = "@toDos";
 
 export default function App() {
   const [working, setWorking] = useState(true);
@@ -19,18 +23,50 @@ export default function App() {
   const work = () => setWorking(true);
   const [text, setText] = useState("");
   const onChangeText = (payload) => setText(payload);
+  const saveToDos = async (toSave) => {
+    await AsyncStorage.setItem(STOAGE_KEY, JSON.stringify(toSave));
+  };
+
+  const loadToDos = async () => {
+    try {
+      const s = await AsyncStorage.getItem(STOAGE_KEY);
+      setToDos(JSON.parse(s));
+    } catch (e) {
+      console.log("에러났어욤~");
+    }
+  };
+
   const [toDos, setToDos] = useState({});
-  const addToDo = () => {
+
+  useEffect(() => {
+    loadToDos();
+  }, []);
+  const addToDo = async () => {
     if (text === "") {
       return;
     }
-    const newToDos = { ...toDos, [Date.now()]: { text, work: working } };
+    const newToDos = { ...toDos, [Date.now()]: { text, working } };
 
     setToDos(newToDos);
-    // save
+    await saveToDos(newToDos);
     setText("");
   };
   console.log(toDos);
+  const deleteToDos = (key) => {
+    Alert.alert("삭제하시겠습니까", "Are you sure?", [
+      {
+        text: "Yes",
+        style : "destructive",
+        onPress: async () => {
+          const newToDos = { ...toDos };
+          delete newToDos[key];
+          setToDos(newToDos);
+          await saveToDos(newToDos);
+        },
+      },
+      { text: "No" },
+    ]);
+  };
 
   return (
     <View style={styles.container}>
@@ -56,17 +92,25 @@ export default function App() {
           value={text}
           onChangeText={onChangeText}
           returnKeyType="done"
-          keyboardType="defalt"
           placeholder={working ? "Add a To Do" : "What is your Schedule"}
           style={styles.input}
         ></TextInput>
       </View>
       <ScrollView>
-        {Object.keys(toDos).map((key) => (
-          <View style={styles.toDo} key={key}>
-            <Text style={styles.toDoText}>{toDos[key].text}</Text>
-          </View>
-        ))}
+        {Object.keys(toDos).map((key) =>
+          toDos[key].working === working ? (
+            <View style={styles.toDo} key={key}>
+              <Text style={styles.toDoText}>{toDos[key].text}</Text>
+              <TouchableOpacity
+                onPress={() => {
+                  deleteToDos(key);
+                }}
+              >
+                <Text>X</Text>
+              </TouchableOpacity>
+            </View>
+          ) : null
+        )}
       </ScrollView>
     </View>
   );
@@ -90,7 +134,7 @@ const styles = StyleSheet.create({
     color: "white",
   },
   input: {
-    marginVertical : 20,
+    marginVertical: 20,
     backgroundColor: "white",
     paddingHorizontal: 20,
     paddingVertical: 15,
@@ -102,11 +146,14 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     paddingHorizontal: 20,
     paddingVertical: 15,
-    borderRadius : 15,
+    borderRadius: 15,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
   },
   toDoText: {
     color: "white",
-    fontSize : 18,
-    fontWeight : 400,
+    fontSize: 18,
+    fontWeight: 400,
   },
 });
